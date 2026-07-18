b20d2058be3104f14400eedbcd20ae64db443ce2
janczesko12 <janczesko12@gmail.com>
2026-07-18 19:23:31 +0200

    Naprawa skanera4

--- scanner-web/static/app.js
+++ scanner-web/static/app.js
@@ -79,19 +79,10 @@ function onScan(decodedText) {
 const scanner = new Html5QrcodeScanner(
     "reader",
     {
-        fps: 15,
         qrbox: {
             width: 320,
             height: 220
         },
-        formatsToSupport: [
-            Html5QrcodeSupportedFormats.EAN_13,
-            Html5QrcodeSupportedFormats.EAN_8,
-            Html5QrcodeSupportedFormats.UPC_A,
-            Html5QrcodeSupportedFormats.UPC_E,
-            Html5QrcodeSupportedFormats.CODE_128,
-            Html5QrcodeSupportedFormats.CODE_39
-        ]
     }
 );
 
