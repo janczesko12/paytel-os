84ce02bc8500e2c417383f0f496598768d9a9446
janczesko12 <janczesko12@gmail.com>
2026-07-18 18:45:16 +0200

    Update app.js

--- scanner-web/static/app.js
+++ scanner-web/static/app.js
@@ -1,3 +1,26 @@
-alert("Secure: " + window.isSecureContext);
-alert("mediaDevices: " + typeof navigator.mediaDevices);
-alert(location.href);
+const video = document.getElementById("video");
+
+async function startCamera() {
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+            video: {
+                facingMode: {
+                    ideal: "environment"
+                }
+            },
+            audio: false
+        });
+
+        video.srcObject = stream;
+
+        await video.play();
+
+        alert("✅ Kamera działa");
+
+    } catch (e) {
+        alert(e.name + "\n" + e.message);
+        console.error(e);
+    }
+}
+
+startCamera();
